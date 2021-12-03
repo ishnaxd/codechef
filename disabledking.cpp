6e1c91cc9cc149e18dac37b55c7ce535c966d4ee
ishnaxd <ishnaxd@gmail.com>
2021-12-04 00:30:43 +0530

    disabledking_codechef

---
+++ disabledking.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+       
+       int n;
+       cin>>n;
+        int steps=0;
+        if((n-1)%2==0){
+            cout<<n-1<<endl;
+        }
+        else{
+            cout<<n<<endl;
+
+        }
+
+        
+    }
+    return 0;
+}
